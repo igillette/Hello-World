7fa67cd5364efaf4520e25c77f496d7d221fc792
igillette <35786075+igillette@users.noreply.github.com>
2018-01-24 19:10:11 -0600

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,17 @@
+/*
+ * Name: Isaac Gillette
+ * Date: 
+ * Description of program: Create a simple Hello World program
+*/
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+
+int main (void) {
+	
+	printf("Isaac");
+
+return 0;
+}
